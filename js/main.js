b24f5f2f28f979a3296a4b45ee31a779411b7093
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-04-27 10:34:22 +0500

    news page desktop the end

--- js/main.js
+++ js/main.js
@@ -27,3 +27,30 @@ let swiper = new Swiper(".homeSwiper", {
         },
     },
 });
+
+let swiper2 = new Swiper(".last_news_Swiper", {
+  spaceBetween: 10,
+  slidesPerView: 1.20,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // breakpoints: {
+  //     320: {
+  //       slidesPerView: 2.15,
+  //       spaceBetween: 10,
+  //     },
+  //     576: {
+  //         slidesPerView: 3,
+  //         spaceBetween: 10,
+  //     },
+  //     768: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 20,
+  //     },
+
+  //     1200: {
+  //       slidesPerView: 5,
+  //       spaceBetween: 20,
+  //     },
+  // },
+});
