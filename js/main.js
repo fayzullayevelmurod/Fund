9681febf461b33ba1e51ef4cb66f06ca637a3dbf
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-04-27 14:19:51 +0500

    burger media the end

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+let burger_btn = document.querySelector('.burger_btn');
+let header_in = document.querySelector('.header_in');
+let media_menu = document.querySelector('.media_menu');
 
+burger_btn.addEventListener('click', () => {
+  header_in.classList.toggle('active');
+  media_menu.classList.toggle('active');
+})
 
+media_menu.addEventListener('click', () => {
+  header_in.classList.remove('active');
+  media_menu.classList.remove('active');
+})
 
 
 let swiper = new Swiper(".homeSwiper", {
